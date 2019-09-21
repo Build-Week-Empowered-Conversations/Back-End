ecfd31b237dcd8d41be004bfb661afc900c1a81d
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2019-09-21 02:37:19 -0500

    Sanity Commit

--- api/server.js
+++ api/server.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken')
 
 const server = express();
+const accountSid = 'AC69406010d41e9c4fddb3b56368458287';
+const authToken = 'b44bc9632a7796fd2036d3a496f753b1';
+const client = require('twilio')(accountSid, authToken);
 
 server.use(helmet());
 server.use(express.json());
@@ -14,18 +17,18 @@ server.use(cors());
 server.get('/', (req, res) => {
   res.send("It's alive!");
 });
-server.get('/token', (req, res) => {
-  const payload ={
-    subject: 'me',
-    me: "Zach"
-  };
-  const secret = "super secret";
-  const opttions={
-    expiresIn: '8h',
-  }
-  const token = jwt.sign(payload, secret, opttions)
 
-  res.status(200).json(token)
-});
+server.post('/', (req,res)=>{
+  let { senderName, senderPhone, recipientName, recipientPhone } = req.body;
+
+  client.messages
+  .create({
+     body: `Dear ${recipientName}, this is the message from Empowered Conversations. ${senderName} wants to talk to you.`,
+     from: '+13164444881',
+     to: `${recipientPhone}`
+   })
+  .then(message => console.log(message.sid));
+  res.status(200).json({message:"message sent sucessfully"})
+})
 
 module.exports = server;
