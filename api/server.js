24ef91b7037ae8e53140f39d449a347adb4a0818
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2019-09-26 11:49:42 -0500

    Sanity Commit

--- api/server.js
+++ api/server.js
@@ -102,6 +102,21 @@ server.put('/users/:id', (req, res) =>{
   
 })
 
+server.get('/users/:id', (req, res) =>{
+  const {id} = req.params;
+
+  db.findById(id)
+  .then(user => {
+      if(user){
+      res.status(200).json(user)
+      } else{
+          res.status(404).json({ message: "The user with the specified ID does not exist." })
+      }
+  })
+  .catch(err =>{
+      res.status(500).json({ error: "The user information could not be retrieved." })
+  })
+})
 
 
 
